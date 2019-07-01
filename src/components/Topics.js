18bc779cb87e302d5770fca6523550fc16a99026
Adam Batwash <a.batwash@mobizcorp.de>
2019-07-01 20:55:47 +0200

    add react-countup
    add a random posts number

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,20 +1,38 @@
 import React from 'react';
+import CountUp from 'react-countup';
+import {random} from 'lodash/fp';
 
 const Topics = ({isShow, ...props}) => {
   if (!isShow) {
     return null;
   }
 
+  const topics = [
+    'HTML Techniques',
+    'CSS Styling',
+    'Flash Tutorials',
+    'Web Miscellanea',
+    'Sites News',
+    'Web Development'
+  ];
+
   return (
     <div className="topicsWrapper">
       <div className="listWrapper">
         <ul>
-          <li>HTML Techniques</li>
-          <li>CSS Styling</li>
-          <li>Flash Tutorials</li>
-          <li>Web Miscellanea</li>
-          <li>Sites News</li>
-          <li>Web Development</li>
+          {topics.map((topic, index) => (
+            <li key={`${topic}_${index}`}>
+              <span className="topic">{topic}</span>
+              <span className="posts">
+                <CountUp
+                  end={random(0)(100)}
+                  duration={5}
+                  delay={0.5}
+                  suffix=" POSTS"
+                />
+              </span>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
